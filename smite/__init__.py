a8fc511c5ffdf3e9cc1fdb020e766e4c22b61f68
way2muchnoise <jens.baetens@live.be>
2015-04-14 16:08:54 +0200

    adds get_god_ranks
    also applies the Qualifying fix to the base get league function

--- smite/__init__.py
+++ smite/__init__.py
@@ -58,7 +58,11 @@ def get_player_status(dev_id, auth_key, session_id, name):
 
 def get_player_league(dev_id, auth_key, session_id, name):
     player = get_player(dev_id, auth_key, session_id, name)[0]
-    return params.get_league_tier(player['Tier_Conquest'])
+    games = player['LeagueConquest']['Wins'] + player['LeagueConquest']['Losses']
+    if player['Tier_Conquest'] > 1 or games > 9:
+        return params.get_league_tier(player['Tier_Conquest'])
+    else:
+        return 'Qualifying ' + games + '/10 '
 
 
 def get_player_league_with_elo(dev_id, auth_key, session_id, name):
@@ -68,3 +72,19 @@ def get_player_league_with_elo(dev_id, auth_key, session_id, name):
         return params.get_league_tier(player['Tier_Conquest']) + " " + repr(round(player['Rank_Stat']))[:-2]
     else:
         return 'Qualifying ' + games + '/10 ' + repr(round(player['Rank_Stat']))[:-2]
+
+
+def get_god_ranks(dev_id, auth_key, session_id, player_name):
+    method_name = "getgodranks"
+    response_format = "json"
+    timestamp = session.get_timestamp()
+    return request.json(
+        base_url
+        + method_name
+        + response_format + "/"
+        + dev_id + "/"
+        + session.get_signature(dev_id, method_name, auth_key, timestamp) + "/"
+        + session_id + "/"
+        + timestamp + "/"
+        + urllib2.quote(player_name)
+    )
